7bf724d36c0e33c17fd418fecfb8bfd97fbb36f7
Kampherime <maugorbit@gmail.com>
2024-07-11 14:40:16 -0400

    added parent nodes and more tests for htmlnodes

--- static_site/src/htmlnode.py
+++ static_site/src/htmlnode.py
@@ -1,9 +1,10 @@
 class HTMLNode:
-    # Each of the properties defaults to none because there are cases for each. 
+    # Each of the properties defaults to none because there are cases for each.
     '''No tag infers raw text
     No value assumes children
     No children assumes value
     No props has no attributes'''
+
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
@@ -15,7 +16,7 @@ class HTMLNode:
 
     def props_to_html(self):
         return_string = ""
-        if self.props == None:
+        if self.props is None:
             return return_string
         for key, value in self.props.items():
             return_string += f" {key}={value}"
@@ -25,7 +26,8 @@ class HTMLNode:
         print(f"HTML Tag: {self.tag}, Value: {self.value}, Children: {self.children}, Properties: {self.props}")
 
     def __eq__(self, other):
-        if self.tag == other.tag and self.value == other.value and self.children == other.children and self.props == other.props:
+        if (self.tag == other.tag and self.value == other.value and
+           self.children == other.children and self.props == other.props):
             return True
         return False
 
@@ -34,16 +36,39 @@ class LeafNode(HTMLNode):
 
     def __init__(self, tag, value, props=None):
         super().__init__(tag, value, None, props)
-        if self.value == None:
+        if self.value is None:
             raise ValueError("LeafNodes cannot have no value")
 
     def to_html(self):
-        if self.value == None:
+        if self.value is None:
             raise ValueError("LeafNode has no value")
-        elif self.tag == None:
+        elif self.tag is None:
             return self.value
-        elif self.props == None:
-            return f"<{self.tag}>{self.value}</{self.tag}>" 
+        elif self.props is None:
+            return f"<{self.tag}>{self.value}</{self.tag}>"
         else:
             prop_to_html = self.props_to_html()
             return f"<{self.tag} {prop_to_html}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag, children, props):
+        super().__init__(tag, None, children, props)
+        if self.children is None:
+            raise Exception("Parent nodes must have children (they're parent nodes (most parents have kids by definition...))")
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Tag must be provided for ParentNodes")
+        if self.children is None:
+            raise ValueError("ParentNodes must have children.")
+        # trying to return a string representing the HTML tag of the node, and its children, recursively...
+        # each recursion generating a new node instance.
+        return_string = ""
+        for child in self.children:
+            return_string += child.to_html()
+        if self.props is None:
+            return f"<{self.tag}>{return_string}</{self.tag}>"
+        return  f"<{self.tag} {self.props_to_html()}>{return_string}</{self.tag}>"
+
--- static_site/src/test_htmlnode.py
+++ static_site/src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -32,8 +32,11 @@ class TestHTMLNode(unittest.TestCase):
             print(f"Functioning as intended. Exception: {e}")
 
         print(f"Testing links representation in HTML/Test3: {test3.to_html()}")
-        test5 = LeafNode("a", "link2", None, {"href": '"https://www.google.com/"', "test2": '"testing"'})
+        test5 = LeafNode("a", "link2", ({"href": '"https://www.google.com/"', "test2": '"testing"'}))
         print(f"Testing to see if multiple properties function: {test5.to_html()}")
+        
+    def test_parent_node(self):
+        pass
 
 
 
